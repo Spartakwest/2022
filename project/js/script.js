c2d8fc2c55324e11f762cd1ad036380dd46f1e82
Spartakwest <spartakvendetta@gmail.com>
2022-07-13 16:24:32 +0300

    new js

--- project/js/script.js
+++ project/js/script.js
@@ -1,71 +1,132 @@
 "use strict";
+//* знаходимо наші елементи
+// знаходимо екран
 let displayDigit = document.querySelector('.display-digit');
+// знаходимо цифри
 let btnNumbers = document.querySelectorAll(".btn--number");
+// знаходимо оражеві знаки
 let btnOrange = document.querySelectorAll('.btn--orange');
+// знаходимо кнопку стерти все
 let ac = document.querySelector('#ac');
-console.log(ac);
-//рахуємо знаки дій
-let counter = 0;
-// створюємо два числа, над якими проходитиме дія
-let str1 = '';
-let str2 = '';
-let str3 = [];
-let result = 0;
+// знаходимо кнопку із відсотками
+let interest = document.querySelector('#interest');
+// знаходимо знак кому
+let coma = document.querySelector('#coma');
+// знаходимо знак зміни знаку
+let change = document.querySelector('#change')
+// знаходимо дорівнює
+let dor = document.querySelector('#dor')
 
-// відстежуємо нажимання на цифри
+// об'являємо змінні та масиви
+let num1 = '';
+let num2 = '';
+let sign1 = '';
+let sign2 = '';
+let arr1 = [];
+let arr2 = [];
+let formatter = new Intl.NumberFormat('ua');
+
+
+// функція обнулення
+function clearAll() {
+  num1 = '';
+  num2 = '';
+  sign1 = '';
+  sign2 = '';
+  arr1 = [];
+  arr2 = [];
+  displayDigit.textContent = '0';
+}
+// console.log(arr2.length);
+
+// функція обмеження цифр
+function limitation(num) {
+  if (num.length <= 9) {
+    return num;
+  } else {
+    return num.split('').splice(0, 9).join('')
+  }
+}
+
+// вішаємо функцію обнулення на ac
+ac.addEventListener('click', clearAll);
+
+// слухаємо кнопки цифри
 for (let elem of btnNumbers) {
-  elem.addEventListener('click', function num() {
-    if (counter == 0) {
-      str1 += elem.textContent;
-      displayDigit.textContent = str1;
-    } else if (counter == 1) {
-      str2 += elem.textContent;
-      displayDigit.textContent = str2;
-    } else if (counter >= 2) {
-      counter = 1;
-      str1 = result;
-      result = 0;
-      str2 = '';
-      str3.shift();
-      num()
-    }
-
-  })
-}
-// відстежуємо нажимання на знаки
-for (let elem of btnOrange) {
   elem.addEventListener('click', () => {
-    if (str1 != '' && str3.length <= 1) {
-      counter += 1;
-      str3.push(elem.textContent);
-      console.log(str3);
-      if (counter == 2) {
-        if (str3[0] == '+') {
-          result = +str1 + +str2;
-          displayDigit.textContent = result;
-        } else if (str3[0] == '-') {
-          result = +str1 - +str2;
-          displayDigit.textContent = result;
-        } else if (str3[0] == '/') {
-          result = +str1 / +str2;
-          displayDigit.textContent = result;
-        } else if (str3[0] == 'x') {
-          result = +str1 * +str2;
-          displayDigit.textContent = result;
-        }
-        else if (str3[1] == '=') {
-          alert('!')
-          displayDigit.textContent = result;
-        }
-
-      }
+    if (arr2.length === 0) {
+      num1 += elem.textContent;
+      displayDigit.textContent = formatter.format(limitation(num1));
+    } else if (arr2.length === 1) {
+      num2 += elem.textContent;
+      displayDigit.textContent = limitation(num2);
     }
   })
 }
-ac.addEventListener('click', () => {
-  str1 = '';
-  str2 = '';
-  str3 = [];
-  result = 0;
-  displayDigit.textContent = result;
+
+// cлухаємо знаки
+for (let elem of btnOrange) {
+
+  elem.addEventListener('click', () => {
+    if (num1 != '' && arr1.length == 0) {
+      sign1 = elem.dataset.sign;
+      arr1.push(Number(num1));
+      arr2.push(sign1)
+      console.log(arr1, arr2);
+      // alert('1')
+    } else if (arr1.length == 1) {
+      sign2 = elem.dataset.sign;
+      arr1.push(Number(num2));
+      arr2.push(sign2);
+      // alert('2')
+      // console.log(arr1, arr2);
+      let result = 0;
+      if (arr2[0] == '+') {
+        result = arr1[0] + arr1[1];
+      } else if (arr2[0] == '-') {
+        result = arr1[0] - arr1[1];
+      } else if (arr2[0] == '*') {
+        result = arr1[0] * arr1[1];
+      } else if (arr2[0] == '/') {
+        result = arr1[0] / arr1[1];
+      }
+      displayDigit.textContent = result;
+      arr1[0] = result;
+      num2 = '';
+      arr1.pop();
+      arr2.shift();
+      // console.log(arr1, arr2);
+    }
+
+
+  })
+}
+
+// слухаємо дорівнює
+dor.addEventListener('click', function () {
+  if (num1 != '' && arr1.length === 0) {
+    displayDigit.textContent = limitation(num1);
+    // alert('=1')
+  } else if (num2 != '' && arr2.length == 1) {
+    arr1.push(Number(num2));
+    let result = 0;
+    if (arr2[0] == '+') {
+      result = arr1[0] + arr1[1];
+    } else if (arr2[0] == '-') {
+      result = arr1[0] - arr1[1];
+    } else if (arr2[0] == '*') {
+      result = arr1[0] * arr1[1];
+    } else if (arr2[0] == '/') {
+      result = arr1[0] / arr1[1];
+    }
+    displayDigit.textContent = result;
+    num1 = result;
+    num2 = '';
+    arr1 = [];
+    arr2.shift();
+    // console.log(num1, num2, arr1, arr2);
+  }
+})
+displayDigit.addEventListener('change', () => {
+  alert('Змфі')
 })
