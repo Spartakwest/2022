90f7595896b70449bd23c34ba560b93f562b7ffb
Spartakwest <spartakvendetta@gmail.com>
2022-08-16 20:24:01 +0300

    end

--- project/js/script.js
+++ project/js/script.js
@@ -13,21 +13,33 @@
   function getRandom() {
     return Math.floor(Math.random() * 100) + 1;
   };
-
+  inp.addEventListener('input', function (event) {
+    console.log(event.key);
+  })
   btn.addEventListener('click', function () {
     inp.focus();
     if (a == '') {
+      inp.placeholder = 'Введіть число від 1 до 100';
+      inp.style.backgroundColor = '#2c51a2';
       a = getRandom();
     }
     if (inp.value == a) {
-      result.textContent = 'ваш результат: вірно, ви вгадали';
+      inp.style.backgroundColor = '#2c9b95';
+      inp.placeholder = 'Ви вгадали';
       inp.value = '';
       a = '';
-    } else if (inp.value < a) {
-      result.textContent = 'ваш результат: невірно, введіть більше число';
+    } else if (inp.value < a && inp.value >= 1) {
+      inp.placeholder = 'введіть більше число(>>>)';
+      inp.style.backgroundColor = '#f55c44';
       inp.value = '';
-    } else if (inp.value > a) {
-      result.textContent = 'ваш результат: невірно, введіть менше число';
+    } else if (inp.value > a && inp.value <= 100) {
+      inp.placeholder = 'введіть менше число(<<<)';
+      inp.style.backgroundColor = '#f55c44';
+      inp.value = '';
+    } else if (inp.value > 100 || inp.value < 1) {
+      console.log('11s');
+      inp.placeholder = 'Ой нє, ти шось не то ввів';
+      inp.style.backgroundColor = '#000';
       inp.value = '';
     }
   })
