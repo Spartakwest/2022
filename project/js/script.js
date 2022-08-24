7d981a72f670af7a69ed064df789e6f6dd05cb0b
Spartakwest <spartakvendetta@gmail.com>
2022-08-24 11:04:20 +0300

    update

--- project/js/script.js
+++ project/js/script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+
+; (function (determinant) {
+  let div = document.querySelector(determinant);
+  let header = div.querySelector('.header');
+  let tds = div.querySelectorAll('td');
+  let arr = [];
+
+  // функція рандомних чисел від 1 до 100
+  function getRandom() {
+    return Math.ceil(Math.random() * 100) + 1
+  }
+  // функція, яка записує 10 рандомних чисел
+  for (let i = 1; i <= 10; i++) {
+    arr.push(getRandom());
+  }
+  console.log(arr);
+
+  for (let number of arr) {
+    tds[number].style.backgroundColor = 'red'
+  }
+
+  for (let td of tds) {
+    td.addEventListener('click', function () {
+      td.style.backgroundColor = 'green'
+    })
+  }
+})('.div1');
