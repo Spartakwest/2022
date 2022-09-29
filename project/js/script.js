1c15f521d3e97837c634c14eae60103d5296ba31
Spartakwest <spartakvendetta@gmail.com>
2022-09-29 20:34:25 +0300

    new update new folder

--- project/js/script.js
+++ project/js/script.js
@@ -4,14 +4,27 @@ let display = document.querySelector('.display');
 
 let table = document.querySelector('.table');
 let str = '';
+let caps = document.querySelector('.caps');
 
 table.addEventListener('click', function (event){
     let td = event.target.closest('td');
 
-    if(td){
+    if(td && !(td.classList.contains('ac'))){
         console.log(td.innerHTML);
-       str += td.innerHTML;
-}
-display.value = str;
+        if(caps.classList.contains('smart')){
+            str += td.innerHTML.toUpperCase();}
+            else {
+              str += td.innerHTML
+          }
+      } else{
+        console.log('else')
+        if(td.classList.contains('clear')){
+            str = '';
+        } else if(td.classList.contains('caps')){
+            caps.classList.toggle('smart')
+        }
+    }
+
+    display.value = str;
 }
 )
