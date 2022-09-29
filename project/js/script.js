9afdb6be967d85598b97d3c402028e23948f4c41
Spartakwest <spartakvendetta@gmail.com>
2022-09-29 17:24:40 +0300

    new update

--- project/js/script.js
+++ project/js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+let display = document.querySelector('.display');
+
+let table = document.querySelector('.table');
+let str = '';
+
+table.addEventListener('click', function (event){
+    let td = event.target.closest('td');
+
+    if(td){
+        console.log(td.innerHTML);
+       str += td.innerHTML;
+}
+display.value = str;
+}
+)
