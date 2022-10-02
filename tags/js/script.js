c525010156d7fde5d3c6b538462b23778531848d
Spartakwest <spartakvendetta@gmail.com>
2022-10-02 18:28:17 +0300

    tags project

--- tags/js/script.js
+++ tags/js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+let menu = document.querySelector('.menu');
+let menuItems = menu.children;
+let tabs = document.querySelectorAll('.tab');
+
+for(let j = 0; j < 3; j ++){
+    menuItems[j].addEventListener('click', function(){
+        for(let i = 0; i < 3; i ++){
+            menuItems[i].classList.remove('active');
+            tabs[i].classList.remove('active')
+        }
+    menuItems[j].classList.add('active');
+    tabs[j].classList.add('active')
+    });
+}
