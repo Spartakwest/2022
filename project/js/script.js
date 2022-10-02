6f00ba2030a68657e21bf380a96eac0542cddc18
Spartakwest <spartakvendetta@gmail.com>
2022-10-02 12:54:39 +0300

    new project update

--- project/js/script.js
+++ project/js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+let toggles = document.querySelectorAll('.toggle');
+
+for(let toggle of toggles){
+    let spoiler = toggle.parentElement.nextElementSibling;
+    toggle.addEventListener('click', function(){
+        spoiler.classList.toggle('active')
+    })
+}
